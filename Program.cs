42835eebf92c7332987ac63f6ac6e92660fd06c7
Gabriel Lopes <lopesg768@gmail.com>
2022-05-19 20:21:04 -0300

    base code completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using dio_rgp_oop.src.Entities;
+
+Hero arus = new Knight("Arus", 23, "Knight");
+Wizard wizard = new Wizard("Jennica", 23, "White wizard");
+
+Console.WriteLine(wizard.Attack(1));
+Console.WriteLine(wizard.Attack(7));
